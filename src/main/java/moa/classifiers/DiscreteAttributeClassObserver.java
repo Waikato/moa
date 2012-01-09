6bc1bd8eab31687a20fa5720973acd4747d285e7
Albert Bifet <abifet@gmail.com>
2012-01-09 20:32:24 +0100

    Added NumericAttributeClassObserver and DiscreteAttributeClassObserver

---
+++ src/main/java/moa/classifiers/DiscreteAttributeClassObserver.java
@@ -0,0 +1,33 @@
+/*
+ *    DiscreteAttributeClassObserver.java
+ *    Copyright (C) 2007 University of Waikato, Hamilton, New Zealand
+ *    @author Richard Kirkby (rkirkby@cs.waikato.ac.nz)
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package moa.classifiers;
+
+/**
+ * Interface for observing the class data distribution for a discrete (nominal) attribute.
+ * This observer monitors the class distribution of a given attribute.
+ * Used in naive Bayes and decision trees to monitor data statistics on leaves.
+ *
+ * @author Richard Kirkby (rkirkby@cs.waikato.ac.nz)
+ * @version $Revision: 7 $ 
+ */
+public interface DiscreteAttributeClassObserver extends AttributeClassObserver {
+
+   
+}
