25ad6c5eb7d71d8797c6a62c63b43240c91f8d25
Jesse Read <jesse.read@gmail.com>
2014-11-05 09:26:56 +0200

    Restructuring MEKAClassifier

--- moa/src/main/java/moa/classifiers/multilabel/MEKAClassifier.java
+++ moa/src/main/java/moa/classifiers/multilabel/MEKAClassifier.java
@@ -27,6 +27,7 @@ import weka.classifiers.UpdateableClassifier;
 import com.yahoo.labs.samoa.instances.MultiLabelInstance;
 import com.yahoo.labs.samoa.instances.MultiLabelPrediction;
 import com.yahoo.labs.samoa.instances.Prediction;
+import com.yahoo.labs.samoa.instances.SamoaToWekaInstanceConverter;
 import moa.classifiers.MultiLabelLearner;
 import moa.classifiers.MultiTargetRegressor;
 
@@ -49,7 +50,39 @@ public class MEKAClassifier extends WEKAClassifier implements MultiLabelLearner,
         //m_L = (m_L < 0 ? raw_header.classIndex() + 1 : m_L);
         m_L = (m_L < 0 ? raw_header.numOutputAttributes(): m_L);
         super.setModelContext(raw_header);
-    }
+
+		weka.core.Instances D = null;
+		SamoaToWekaInstanceConverter conv = new SamoaToWekaInstanceConverter();
+		try {
+			D = conv.wekaInstances(raw_header);
+		} catch(Exception e) {
+			System.err.println("FAILED TO CONVERT");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		D.setClassIndex(m_L);
+		System.out.println("L="+D.classIndex()+","+m_L);
+		this.instancesBuffer = new weka.core.Instances(D);
+		if (classifier instanceof UpdateableClassifier) {
+			this.instancesBuffer.setClassIndex(m_L);
+			// System.out.println(instancesBuffer.classIndex());
+			// System.out.println(instancesBuffer.classAttribute().name());
+			// System.out.println(instancesBuffer.classAttribute().isNumeric());
+			System.out.println("N="+instancesBuffer.numInstances()+"\nD:\n"+instancesBuffer);
+
+			try {
+				this.classifier.buildClassifier(instancesBuffer);
+			} catch(Exception e) {
+				System.err.println("FAILED TO BUILD");
+				e.printStackTrace();
+				System.exit(1);
+			}
+			this.isClassificationEnabled = true;
+		} else {
+			System.err.println("Only suports UpdateableClassifiers for now.");
+			System.exit(1);
+		}
+	}
 
     @Override
     public void trainOnInstanceImpl(Instance instance) {
@@ -60,31 +93,20 @@ public class MEKAClassifier extends WEKAClassifier implements MultiLabelLearner,
     @Override
     public void trainOnInstanceImpl(MultiLabelInstance samoaInstance) {
         weka.core.Instance inst = this.instanceConverter.wekaInstance(samoaInstance);
+		System.out.println(""+m_L);                   // <--  this is correct
+		System.out.println(""+inst.classIndex());     // <--- this one is wrong
+		inst.setClassIndex(m_L);                      // <-- so, fix it!
+
         if (m_L < 0) {
+			System.out.println("setModelContext(..) has not been called yet!!!");
             m_L = samoaInstance.numOutputAttributes();//inst.classIndex() + 1;
+			System.exit(1);
         }
 
         //System.out.println(inst.classIndex());
-        try {
-            if (numberInstances < 1) { // INIT 
-                weka.core.Instances D = inst.dataset();
-                D.setClassIndex(m_L-1);
-                this.instancesBuffer = new weka.core.Instances(D);
-                if (classifier instanceof UpdateableClassifier) {
-                    this.instancesBuffer.setClassIndex(m_L-1);
-                   // System.out.println(instancesBuffer.classIndex());
-                   // System.out.println(instancesBuffer.classAttribute().name());
-                   // System.out.println(instancesBuffer.classAttribute().isNumeric());
-                    this.classifier.buildClassifier(instancesBuffer);
-                    this.isClassificationEnabled = true;
-                } else {
-                    System.err.println("Only suports UpdateableClassifiers for now.");
-                    System.exit(1);
-                }
-            } else { // UPDATE
-                ((UpdateableClassifier) classifier).updateClassifier(inst);
-            }
-            numberInstances++;
+		try {
+			System.out.println("UPDATE   WITH instances of "+instancesBuffer.classIndex()+" labels :\n"+inst);
+			((UpdateableClassifier) classifier).updateClassifier(inst);
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
@@ -102,7 +124,7 @@ public class MEKAClassifier extends WEKAClassifier implements MultiLabelLearner,
        Prediction prediction = new MultiLabelPrediction(predictionArray.length);
        for (int j = 0; j < predictionArray.length; j++){
             prediction.setVote(j, 1, predictionArray[j]);
-			//prediction.setVote(j, 0, 1. - predictionArray[j]);
+			prediction.setVote(j, 0, 1. - predictionArray[j]);
         }
         return prediction;
     }
