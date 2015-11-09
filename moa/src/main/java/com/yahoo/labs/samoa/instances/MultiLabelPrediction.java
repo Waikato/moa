12f6b0c1c44a35951b8bf07156699485767cfbec
Aljaž Osojnik <aljaz.osojnik@ijs.si>
2015-11-09 15:22:23 +0100

    Removed merging artefacts

--- moa/src/main/java/com/yahoo/labs/samoa/instances/MultiLabelPrediction.java
+++ moa/src/main/java/com/yahoo/labs/samoa/instances/MultiLabelPrediction.java
@@ -33,10 +33,7 @@ public class MultiLabelPrediction implements Prediction, Serializable {
 	public MultiLabelPrediction(DoubleVector[] prediction) {
 		this.prediction = prediction;
 	}
-	
-	public MultiLabelPrediction(DoubleVector[] prediction) {
-		this.prediction = prediction;
-	}
+
 	
 	@Override
 	public int numOutputAttributes() {
