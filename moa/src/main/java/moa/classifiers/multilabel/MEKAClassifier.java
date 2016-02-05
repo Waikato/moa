db0e2e90881b90106d643a84a7c90c522b43c586
Jesse Read <jesse.read@gmail.com>
2016-02-05 10:22:14 +0100

    test

--- moa/src/main/java/moa/classifiers/multilabel/MEKAClassifier.java
+++ moa/src/main/java/moa/classifiers/multilabel/MEKAClassifier.java
@@ -81,6 +81,7 @@ public class MEKAClassifier extends AbstractMultiLabelLearner implements MultiTa
             //System.out.println(baseLearnerOption.getValue());
             String[] options = weka.core.Utils.splitOptions(baseLearnerOption.getValueAsCLIString());
             createWekaClassifier(options);
+			System.out.println(""+this.classifier.m_Classifier);
         } catch (Exception e) {
             System.err.println("Creating a new classifier: " + e.getMessage());
         }
