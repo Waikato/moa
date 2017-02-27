eb4b7f7aa1f48f8fed5f79bd35094285f426bdfe
crekowski <cornelius.vonrekowski@gmail.com>
2017-02-27 14:35:39 +0100

    Create abstract class ALMainTask
    This abstract class will be used to group all active learning tasks
    so that they can easily be searched.

---
+++ moa/src/main/java/moa/tasks/active/ALMainTask.java
@@ -0,0 +1,34 @@
+/*
+ *    ALMainTask.java
+ *    Copyright (C) 2017 Otto-von-Guericke-University, Magdeburg, Germany
+ *    @author Cornelius Styp von Rekowski (cornelius.styp@ovgu.de)
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *    
+ */
+package moa.tasks.active;
+
+import moa.tasks.MainTask;
+
+/**
+ * This class provides a superclass for Active Learning tasks, which 
+ * enables convenient searching for those tasks for example when showing 
+ * a list of available Active Learning tasks.
+ * 
+ * @author Cornelius Styp von Rekowski (cornelius.styp@ovgu.de)
+ * @version $Revision: 1 $
+ */
+public abstract class ALMainTask extends MainTask {
+	
+}
