96715c3ac723c2f9b87364ce14a347aeb72478ca
tpham93 <tuan.pham@st.ovgu.de>
2017-03-17 22:08:18 +0100

    Add documentation

--- moa/src/main/java/moa/classifiers/active/budget/IncrementalQuantileFilter.java
+++ moa/src/main/java/moa/classifiers/active/budget/IncrementalQuantileFilter.java
@@ -1,3 +1,22 @@
+/*
+ *    IncrementalQuantileFilter.java
+ *    Copyright (C) 2017 Otto-von-Guericke-University, Magdeburg, Germany
+ *    @author Tuan Pham Minh (tuan.pham@ovgu.de)
+ *    
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *    
+ */
 package moa.classifiers.active.budget;
 
 import java.util.List;
@@ -8,6 +27,19 @@ import moa.core.ObjectRepository;
 import moa.options.AbstractOptionHandler;
 import moa.tasks.TaskMonitor;
 
+/**
+ * This budget manager is an implementation of the incremental quantile filter
+ * proposed in [1]
+ *
+ *
+ * [1]	Kottke D., Krempl G., Spiliopoulou M. 
+ * 		(2015) Probabilistic Active Learning in Datastreams. 
+ * 		In: Fromont E., De Bie T., van Leeuwen M. (eds) 
+ * 		Advances in Intelligent Data Analysis XIV. 
+ * 		Lecture Notes in Computer Science, vol 9385. Springer, Cham
+ * @author Tuan Pham Minh (tuan.pham@ovgu.de)
+ * @version $Revision: 1 $
+ */
 public class IncrementalQuantileFilter extends AbstractOptionHandler implements BudgetManager {
 
 	private static final long serialVersionUID = 1L;
--- moa/src/main/java/moa/classifiers/active/budget/Ranking.java
+++ moa/src/main/java/moa/classifiers/active/budget/Ranking.java
@@ -1,3 +1,22 @@
+/*
+ *    Ranking.java
+ *    Copyright (C) 2017 Otto-von-Guericke-University, Magdeburg, Germany
+ *    @author Tuan Pham Minh (tuan.pham@ovgu.de)
+ *    
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *    
+ */
 package moa.classifiers.active.budget;
 
 import java.util.ArrayList;
@@ -11,6 +30,13 @@ import java.util.List;
  * @version $Revision: 1 $
  */
 public class Ranking<T extends Comparable<T>> {
+
+	/**
+	 * This class is used to sort indices by data from another array.
+	 * 
+	 * @author Tuan Pham Minh (tuan.pham@ovgu.de)
+	 * @version $Revision: 1 $
+	 */
 	private class IndexComparator implements Comparator<Integer>
 	{
 		List<T> data;
@@ -25,7 +51,12 @@ public class Ranking<T extends Comparable<T>> {
 		}
 	}
 	
-
+	/**
+	 * return the index in a sorted state
+	 * @param data the data which should be sorted
+	 * @param index the index from which the index in the sorted state should be returned
+	 * @return the index in the sorted state of the given instance
+	 */
 	public int rank(List<T> data, int index)
 	{
 		Comparator<Integer> comparator = new IndexComparator(data);
@@ -34,6 +65,11 @@ public class Ranking<T extends Comparable<T>> {
 		return indexList.indexOf(index);
 	}
 	
+	/**
+	 * generates a list containing indices from an array's length
+	 * @param numData the array's length
+	 * @return a list with all indices
+	 */
 	private List<Integer> generateIndexList(int numData)
 	{
 		List<Integer> list = new ArrayList<>();
