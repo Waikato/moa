Waikato/moa

commit 0759d6ce4715c68e5441469d6217f9b50e0cede2

Implement scalable x-axis for param graphs
The x-axis for the param graph has been a dummy so far. The new implementation allows flexible scaling, realised by abstractions.

moa/src/main/java/moa/gui/visualization/ParamGraphCanvas.java
@@ -20,6 +20,7 @@
 package moa.gui.visualization;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import moa.evaluation.MeasureCollection;
 
@@ -34,6 +35,8 @@ import moa.evaluation.MeasureCollection;
 public class ParamGraphCanvas extends AbstractGraphCanvas {
 
 	private static final long serialVersionUID = 1L;
+	
+	private double[] variedParamValues;
 
 	/**
 	 * Initialises a ProcessGraphCanvas by calling the super constructor with a
@@ -56,9 +59,33 @@ public class ParamGraphCanvas extends AbstractGraphCanvas {
 			String variedParamName, double[] variedParamValues, Color[] colors) {
 		this.measures = measures;
 		this.measureSelected = mSelect;
+		this.variedParamValues = variedParamValues;
 		((GraphScatter) this.plotPanel).setGraph(measures, mSelect, 
 				variedParamName, variedParamValues, colors);
 		updateCanvas(false);
+	}
+	
+	@Override
+	public double getMaxXValue() {
+		double max = Double.MIN_VALUE;
+
+		for (int i = 0; i < this.variedParamValues.length; i++) {
+			if (this.variedParamValues[i] > max) {
+				max = this.variedParamValues[i];
+			}
+		}
+		return max;
+	}
+
+	@Override
+	public void setSize() {
+		setSize((int) (baseWidth  * (1/x_resolution)),
+				(int) (baseHeight * y_resolution));
+	}
+
+	@Override
+	public void setPreferredSize() {
+		setPreferredSize(new Dimension(getWidth(), getHeight()));		
 	}
 
 }

moa/src/main/java/moa/gui/visualization/AbstractGraphAxes.java
@@ -35,7 +35,7 @@ import javax.swing.JPanel;
  * @version $Revision: 1 $
  * @see GraphAxes
  */
-public abstract class AbstractGraphAxes extends JPanel{
+public abstract class AbstractGraphAxes extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
 	protected static final int X_OFFSET_LEFT = 35;
@@ -49,16 +49,20 @@ public abstract class AbstractGraphAxes extends JPanel{
     protected double x_resolution;
     protected double y_resolution;
     
-    protected double max_value;
-    protected double upper_value;
+    protected double max_x_value;
+    protected double max_y_value;
+    protected double upper_x_value;
+    protected double upper_y_value;
 
     /**
      * Initialises a AbstractGraphAxes by setting the initial values and the
      * layout.
      */
 	public AbstractGraphAxes() {
-		this.max_value = 1;
-		this.upper_value = 1;
+		this.max_x_value = 1;
+		this.max_y_value = 1;
+		this.upper_x_value = 1;
+		this.upper_y_value = 1;
     	
     	javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -156,7 +160,7 @@ public abstract class AbstractGraphAxes extends JPanel{
         
         for (int i = 0; i <= numLabels; i++) {
         	double fraction = i/numLabels;
-        	double value = fraction*upper_value;
+        	double value = fraction*upper_y_value;
         	g.drawString(d.format(value), 1, (int) ((1-fraction)*height) + Y_OFFSET_TOP+5);
         	g.drawLine(X_OFFSET_LEFT - 5, (int) ((1-fraction)*height) + Y_OFFSET_TOP, 
         			   X_OFFSET_LEFT, (int) ((1-fraction)*height) + Y_OFFSET_TOP);
@@ -169,11 +173,23 @@ public abstract class AbstractGraphAxes extends JPanel{
      * @param max maximum y value
      */
     public void setYMaxValue(double max){
-        this.max_value = max;
+        this.max_y_value = max;
+    }
+    
+    /**
+     * Sets the maximum x value
+     * @param max maximum x value
+     */
+    public void setXMaxValue(double max){
+        this.max_x_value = max;
     }
     
     public void setYUpperValue(double value) {
-    	this.upper_value = value;
+    	this.upper_y_value = value;
+    }
+    
+    public void setXUpperValue(double value) {
+    	this.upper_x_value = value;
     }
 
     /**
@@ -182,7 +198,7 @@ public abstract class AbstractGraphAxes extends JPanel{
      * @return position on the y axis
      */
     private int calcY(double value){
-        return (int)(height-(value/max_value)*height)+Y_OFFSET_TOP;
+        return (int)(height-(value/max_y_value)*height)+Y_OFFSET_TOP;
     }
 
 }

moa/src/main/java/moa/gui/visualization/AbstractGraphCanvas.java
@@ -19,7 +19,6 @@
  */
 package moa.gui.visualization;
 
-import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
@@ -55,23 +54,24 @@ public abstract class AbstractGraphCanvas extends JPanel {
 
 	protected AbstractGraphPlot plotPanel;
 
-	private static final int X_OFFSET_LEFT = 35;
+	protected static final int X_OFFSET_LEFT = 35;
 
-	private static final int X_OFFSET_RIGHT = 5;
+	protected static final int X_OFFSET_RIGHT = 5;
 
-	private static final int Y_OFFSET_BOTTOM = 20;
+	protected static final int Y_OFFSET_BOTTOM = 20;
 
-	private static final int Y_OFFSET_TOP = 20;
+	protected static final int Y_OFFSET_TOP = 20;
 
-	private double max_y_value;
+	protected double max_y_value;
 
-	private int max_x_value;
+	protected double max_x_value;
 
-	private double x_resolution;
+	protected double x_resolution;
 
-	private double y_resolution;
+	protected double y_resolution;
 
-	private double baseHeight;
+	protected double baseHeight;
+	protected double baseWidth;
 
 	/**
 	 * Initialises an AbstractGraphCanvas by constructing its AbstractGraphAxes,
@@ -101,7 +101,7 @@ public abstract class AbstractGraphCanvas extends JPanel {
 	/**
 	 * Updates the base height, which is used to determine the canvas size. It
 	 * is defined as the current height divided by the y_resolution. To prevent
-	 * unnecessary scrolling after reduzing the y_resolution, the baseHeight is
+	 * unnecessary scrolling after reducing the y_resolution, the baseHeight is
 	 * reset to its initial value of 111 on y_resolution = 1.
 	 */
 	private void updateBaseHeight() {
@@ -109,6 +109,14 @@ public abstract class AbstractGraphCanvas extends JPanel {
 			this.baseHeight = getHeight() / y_resolution;
 		} else {
 			this.baseHeight = 111;
+		}
+	}
+	
+	private void updateBaseWidth() {
+		if ((1/x_resolution) > 1) {
+			this.baseWidth = getWidth() / (1/x_resolution);
+		} else {
+			this.baseWidth = 500;
 		}
 	}
 
@@ -160,13 +168,18 @@ public abstract class AbstractGraphCanvas extends JPanel {
 	 */
 	public void updateCanvas(boolean force) {
 		if (updateMaxValues() || force) {
-			setSize(getWidth(), (int) (this.baseHeight * y_resolution));
-			setPreferredSize(new Dimension(
-					(int) Math.max(getPreferredSize().getWidth(), (int) (max_x_value / x_resolution) + X_OFFSET_LEFT),
-					getHeight()));
+			setSize();
+			setPreferredSize();
+//			setSize(getWidth(), (int) (this.baseHeight * y_resolution));
+//			setPreferredSize(new Dimension(
+//					(int) Math.max(getPreferredSize().getWidth(), (int) (max_x_value / x_resolution) + X_OFFSET_LEFT),
+//					getHeight()));
 			this.repaint();
 		}
 	}
+	
+	public abstract void setSize();
+	public abstract void setPreferredSize();
 
 	/**
 	 * Computes the maximum value of the underlying measures at the currently
@@ -184,23 +197,8 @@ public abstract class AbstractGraphCanvas extends JPanel {
 		}
 		return max;
 	}
-
-	/**
-	 * Computes the maximum number of values of the underlying measures at the
-	 * currently selected measure.
-	 * 
-	 * @return max number of values at measureSelected
-	 */
-	private int maxNumValues() {
-		int max = 0;
-
-		for (int i = 0; i < this.measures.length; i++) {
-			if (this.measures[i].getNumberOfValues(this.measureSelected) > max) {
-				max = this.measures[i].getNumberOfValues(this.measureSelected);
-			}
-		}
-		return max;
-	}
+	
+	public abstract double getMaxXValue();
 
 	/**
 	 * Computes the maximum values of the registered measure collections and
@@ -210,7 +208,7 @@ public abstract class AbstractGraphCanvas extends JPanel {
 	 */
 	private boolean updateMaxValues() {
 
-		int max_x_value_new;
+		double max_x_value_new;
 		double max_y_value_new;
 
 		if (this.measures == null) {
@@ -218,7 +216,7 @@ public abstract class AbstractGraphCanvas extends JPanel {
 			max_x_value_new = 1;
 			max_y_value_new = 1;
 		} else {
-			max_x_value_new = maxNumValues();
+			max_x_value_new = getMaxXValue();
 			max_y_value_new = maxValue();
 		}
 
@@ -227,7 +225,9 @@ public abstract class AbstractGraphCanvas extends JPanel {
 			this.max_x_value = max_x_value_new;
 			this.max_y_value = max_y_value_new;
 			updateMaxYValue();
+			updateMaxXValue();
 			updateYUpperValue();
+			updateXUpperValue();
 			return true;
 		}
 		return false;
@@ -256,6 +256,14 @@ public abstract class AbstractGraphCanvas extends JPanel {
 		plotPanel.setYMaxValue(max_y_value);
 	}
 	
+	/**
+	 * Updates the max x value of the axes and plot panel.
+	 */
+	private void updateMaxXValue() {
+		axesPanel.setXMaxValue(max_x_value);
+		plotPanel.setXMaxValue(max_x_value);
+	}
+	
 	private void updateYUpperValue() {
 		int digits_y = (int)(Math.log10(max_y_value))-1;
         double upper = Math.ceil(max_y_value/Math.pow(10,digits_y));
@@ -267,6 +275,19 @@ public abstract class AbstractGraphCanvas extends JPanel {
         
         this.axesPanel.setYUpperValue(upper);
         this.plotPanel.setYUpperValue(upper);
+	}
+	
+	private void updateXUpperValue() {
+		int digits_x = (int)(Math.log10(max_x_value))-1;
+        double upper = Math.ceil(max_x_value/Math.pow(10,digits_x));
+        if(digits_x < 0) upper*=Math.pow(10,digits_x);
+
+        if(Double.isNaN(upper)) {
+        	upper = 1.0;
+        }
+        
+        this.axesPanel.setXUpperValue(upper);
+        this.plotPanel.setXUpperValue(upper);
 	}
 
 	/**
@@ -284,6 +305,7 @@ public abstract class AbstractGraphCanvas extends JPanel {
 	@Override
 	protected void paintChildren(Graphics g) {
 		updateBaseHeight();
+		updateBaseWidth();
 		updateChildren();
 		super.paintChildren(g);
 	}

moa/src/main/java/moa/gui/visualization/AbstractGraphPlot.java
@@ -39,8 +39,10 @@ public abstract class AbstractGraphPlot extends JPanel{
 	
 	private static final long serialVersionUID = 1L;
 	
-    protected double max_value;
-    protected double upper_value;
+    protected double max_x_value;
+    protected double max_y_value;
+    protected double upper_x_value;
+    protected double upper_y_value;
     protected MeasureCollection[] measures;
     protected int measureSelected = 0;
 
@@ -49,8 +51,10 @@ public abstract class AbstractGraphPlot extends JPanel{
     protected Color[] colors;
 
 	public AbstractGraphPlot() {
-		this.max_value = 1;
-		this.upper_value = 1;
+		this.max_x_value = 1;
+		this.max_y_value = 1;
+		this.upper_x_value = 1;
+		this.upper_y_value = 1;
     	this.measureSelected = 0;
     	
     	setOpaque(false);
@@ -82,16 +86,27 @@ public abstract class AbstractGraphPlot extends JPanel{
 	}
 	
     /**
-     * Sets minimum and maximum y value.
-     * @param min minimum y value
+     * Sets maximum y value.
      * @param max maximum y value
      */
     protected void setYMaxValue(double max){
-        this.max_value = max;
+        this.max_y_value = max;
     }
     
+    /**
+     * Sets maximum x value.
+     * @param max maximum x value
+     */
+    protected void setXMaxValue(double max){
+        this.max_x_value = max;
+    }
+    
+    protected void setXUpperValue(double value) {
+    	this.upper_x_value = value;
+    }  
+    
     protected void setYUpperValue(double value) {
-    	this.upper_value = value;
+    	this.upper_y_value = value;
     }
 
     /**

moa/src/main/java/moa/gui/visualization/GraphMultiCurve.java
@@ -102,7 +102,7 @@ public class GraphMultiCurve extends AbstractGraphPlot {
 
             for (int i = 0; i < n; i ++) {
             	x[i] = (int) (i / x_resolution);
-            	y[i] = (int)(height-(m.getValue(mSelect, i)/this.upper_value)*height);
+            	y[i] = (int)(height-(m.getValue(mSelect, i)/this.upper_y_value)*height);
             }
             g.setColor(color);
             g.drawPolyline(x, y, n);

moa/src/main/java/moa/gui/visualization/GraphScatter.java
@@ -82,14 +82,14 @@ public class GraphScatter extends AbstractGraphPlot {
     	int height = getHeight();
     	int width = getWidth();
     	
-    	int x = (int) (width * variedParamValue);
+    	int x = (int)((variedParamValue / this.upper_x_value) * width); 
    		double value = m.getLastValue(this.measureSelected);  
         if(Double.isNaN(value)){
         	// no result for this budget yet
             return;
         }
 
-        int y = (int)(height - (value / this.upper_value) * height); 
+        int y = (int)(height - (value / this.upper_y_value) * height); 
         
         g.setColor(color);
     	g.fillOval(x - DOT_SIZE/2, y - DOT_SIZE/2, DOT_SIZE, DOT_SIZE);

moa/src/main/java/moa/gui/visualization/ParamGraphAxes.java
@@ -19,6 +19,7 @@
  */
 package moa.gui.visualization;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.text.DecimalFormat;
 
@@ -33,18 +34,31 @@ import java.text.DecimalFormat;
 public class ParamGraphAxes extends AbstractGraphAxes {
 
 	private static final long serialVersionUID = 1L;
-
+	
 	@Override
 	protected void drawXLabels(Graphics g) {
-		// TODO implement
-    	DecimalFormat d = new DecimalFormat("0.00");
-    	for (double i = 0.0; i <= 1.0; i += 0.2) {
-    		int x = (int) (this.width * i) + X_OFFSET_LEFT;
-    		g.drawLine(x, this.height + Y_OFFSET_TOP, x, this.height+Y_OFFSET_TOP + 5);
-    		String label = d.format(i);
-    		int str_length = g.getFontMetrics().stringWidth(label);
-            g.drawString(label, x - str_length/2, this.height + Y_OFFSET_TOP + 18);
-    	}
+        g.setColor(Color.BLACK);
+
+        //x-axis markers + labels
+        DecimalFormat d = new DecimalFormat("0.00");
+
+        double numLabels = Math.min(Math.pow(2,  (int) (1/x_resolution)), 32);
+        /*
+         * technically, this is numLabels-1, but as we're iterating 
+         * 0 <= i <= numLabels, we need the extra label. Also don't draw more
+         * than 32 labels.
+         */
+        
+        for (int i = 0; i <= numLabels; i++) {
+        	double fraction = i/numLabels;
+        	double value = fraction*upper_x_value;
+        	String label = d.format(value);
+        	int str_length = g.getFontMetrics().stringWidth(label);
+        	
+        	g.drawString(label, (int) (fraction*width) + X_OFFSET_LEFT - str_length/2, height+Y_OFFSET_TOP+18);
+        	g.drawLine((int) (fraction*width) + X_OFFSET_LEFT, height+Y_OFFSET_TOP, 
+        			   (int) (fraction*width) + X_OFFSET_LEFT, height+Y_OFFSET_TOP+5);
+        }
 	}
 
 }

moa/src/main/java/moa/gui/visualization/ProcessGraphCanvas.java
@@ -20,6 +20,7 @@
 package moa.gui.visualization;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import moa.evaluation.MeasureCollection;
 
@@ -90,6 +91,30 @@ public class ProcessGraphCanvas extends AbstractGraphCanvas {
 		((GraphMultiCurve) this.plotPanel).setProcessFrequency(min_processFrequency);
 		((GraphMultiCurve) this.plotPanel).setGraph(measures, mSelect, processFrequencies, colors);
 		updateCanvas(false);
+	}
+	
+	@Override
+	public double getMaxXValue() {
+		int max = 0;
+
+		for (int i = 0; i < this.measures.length; i++) {
+			if (this.measures[i].getNumberOfValues(this.measureSelected) > max) {
+				max = this.measures[i].getNumberOfValues(this.measureSelected);
+			}
+		}
+		return max;
+	}
+
+	@Override
+	public void setSize() {
+		setSize(getWidth(), (int) (baseHeight * y_resolution));	
+	}
+
+	@Override
+	public void setPreferredSize() {
+		setPreferredSize(new Dimension(
+				(int) Math.max(getPreferredSize().getWidth(), (int) (max_x_value / x_resolution) + X_OFFSET_LEFT),
+				getHeight()));
 	}
 
 }

commit 18f96a102d4686e800e25ddb446e0416df3ef98d

Fix ALPQE tasks fail
This fixes a bug introduced in 0759d6ce. If a single ALPQE is executed, the variedParamValues field is null, which was not catched in the new getMaxXValue() method.

moa/src/main/java/moa/gui/visualization/ParamGraphCanvas.java
@@ -71,6 +71,10 @@ public class ParamGraphCanvas extends AbstractGraphCanvas {
 
     @Override
     public double getMaxXValue() {
+        if (this.variedParamValues == null) {
+            // no task with a varied param
+            return 0;
+        }
         double max = Double.MIN_VALUE;
 
         for (int i = 0; i < this.variedParamValues.length; i++) {