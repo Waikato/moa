585577f69e122a1405d4f7dc2feeed8acab377bb
hmgomes <heitor_murilo_gomes@yahoo.com.br>
2017-06-20 09:50:58 +0200

    Fix to ADWINChangeDetector code

--- moa/src/main/java/moa/classifiers/core/driftdetection/ADWINChangeDetector.java
+++ moa/src/main/java/moa/classifiers/core/driftdetection/ADWINChangeDetector.java
@@ -45,7 +45,12 @@ public class ADWINChangeDetector extends AbstractChangeDetector {
         if (this.adwin == null) {
             resetLearning();
         }
-        this.isChangeDetected = adwin.setInput(inputValue);
+        double ErrEstim = this.adwin.getEstimation();
+        if(adwin.setInput(inputValue)) {
+            if (this.adwin.getEstimation() > ErrEstim) {
+                this.isChangeDetected = true;
+            }
+        }
         this.isWarningZone = false;
         this.delay = 0.0;
         this.estimation = adwin.getEstimation();
