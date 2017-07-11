0fde631f4c13541f9ecf77339e1061b2e53ec244
hmgomes <heitor_murilo_gomes@yahoo.com.br>
2017-07-11 15:25:32 +0200

    Corrected spelling for numFoldsOption description

--- moa/src/main/java/moa/tasks/EvaluatePrequentialDelayedCV.java
+++ moa/src/main/java/moa/tasks/EvaluatePrequentialDelayedCV.java
@@ -105,7 +105,7 @@ public class EvaluatePrequentialDelayedCV extends MainTask {
             "File to append intermediate csv results to.", null, "csv", true);
 
     public IntOption numFoldsOption = new IntOption("numFolds", 'w',
-            "The number of folds (e.g. distributed models) to be use.", 10, 1, Integer.MAX_VALUE);
+            "The number of folds (e.g. distributed models) to be used.", 10, 1, Integer.MAX_VALUE);
 
     public MultiChoiceOption validationMethodologyOption = new MultiChoiceOption(
             "validationMethodology", 'a', "Validation methodology to use.", new String[]{
