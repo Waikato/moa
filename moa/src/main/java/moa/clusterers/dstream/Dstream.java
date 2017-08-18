6c5ffb07c77445c056740906cc71c6e0a3e63b5b
richard-moulton <richard.hugh.m@gmail.com>
2017-08-18 14:44:27 -0400

    Added license headers to all D-Stream files. Ensured gap cannot equal zero.
    Added the Apache License to each of the D-Stream files for inclusion in MOA.
    
    Adjusted procedure to set gap in Dstream.java. The variable gap can no longer be calculated as zero. If this is the case, then the value of gap is set to 1.

--- moa/src/main/java/moa/clusterers/dstream/Dstream.java
+++ moa/src/main/java/moa/clusterers/dstream/Dstream.java
@@ -1,7 +1,19 @@
 /*
  *    Dstream.java
  *    
- *    @author Richard Hugh Moulton    
+ *    @author Richard Hugh Moulton  (rmoul026 -[at]- uottawa dot ca)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.  
  */
 
 package moa.clusterers.dstream;
@@ -299,6 +311,11 @@ public class Dstream extends AbstractClusterer {
 			double optionA = this.cl/this.cm;
 			double optionB = ((double)this.N-this.cm)/((double)this.N-this.cl);
 			gap = (int)Math.floor(Math.log(Math.max(optionA, optionB))/Math.log(this.getDecayFactor()));
+			// Ensure that gap is not zero (i.e. if the procedure to calculate gap rounds down to zero, then set gap to 1 and adjust clustering every instance)
+			if(gap == 0)
+			{
+				gap = 1;
+			}
 			//System.out.println(" A is "+optionA+", B is "+optionB+" and gap = "+gap);
 		}
 
