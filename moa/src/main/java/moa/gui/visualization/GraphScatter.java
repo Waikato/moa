Waikato/moa

commit 5ac991eaf07a299096ff25a4fb71263d28b44c40

Simplify standard deviation display
The previous implementation of displaying the standard deviation was not flexible enough and has been simplified.
This commit also includes several simplification in the graphics pipeline, such as the removal of storing the selected measure index multiple times.

moa/src/main/java/moa/gui/visualization/GraphScatter.java
@@ -40,23 +40,17 @@ public class GraphScatter extends AbstractGraphPlot {
     
     private double[] variedParamValues;
     
-    private boolean paintCurrentParam;
-    
-    public GraphScatter(boolean b) {
-        this.paintCurrentParam = b;
-    }
-	
     /**
      * Draws a scatter graph based on the varied parameter and the measures.
-     * @param measures  list of measure collections, one for each task
-     * @param mSelect currently selected measure
-     * @param variedParamName name of the varied parameter
+     * @param measures list of measure collections, one for each task
+     * @param stds standard deviation values
      * @param variedParamValues values of the varied parameter
+     * @param colors color encodings for the different tasks
      */
-    public void setGraph(MeasureCollection[] measures, int mSelect, 
+    public void setGraph(MeasureCollection[] measures, MeasureCollection[] stds, 
     		double[] variedParamValues, Color[] colors) {
         this.variedParamValues = variedParamValues;
-        super.setGraph(measures, mSelect, colors);
+        super.setGraph(measures, stds, colors);
     }
 
     @Override
@@ -67,43 +61,36 @@ public class GraphScatter extends AbstractGraphPlot {
         	// no measures received yet -> nothing to paint
         	return; 
         }
-        
-        // scatter current budgets
+
+        // scatter current param values
         for (int i = 0; i < this.measures.length; i++) {
-            if (this.paintCurrentParam) {
-                this.scatterCurrentParam(g, this.measures[i], this.colors[i%this.colors.length]);
-            } else {
-                this.scatter(g, this.measures[i], this.variedParamValues[i], this.colors[i]);
-            }
+                this.scatter(g, i);
         }
     }
     
     /**
      * Paint a dot onto the panel.
      * @param g graphics object
-     * @param m MeasureCollection containing the data
+     * @param i index of the varied parameter
      */
-    private void scatter(Graphics g, MeasureCollection m, double variedParamValue, Color color) {
+    private void scatter(Graphics g, int i) {
 
     	int height = getHeight();
     	int width = getWidth();
     	
-    	int x = (int)(((m.getLastValue(6) - this.lower_x_value) / (this.upper_x_value - this.lower_x_value)) * width); 
-    	double value = m.getLastValue(this.measureSelected);  
+    	int x = (int)(((this.variedParamValues[i] - this.lower_x_value) / (this.upper_x_value - this.lower_x_value)) * width); 
+    	double value = this.measures[i].getLastValue(this.measureSelected);  
 
         if(Double.isNaN(value)){
         	// no result for this budget yet
             return;
         }
 
-        int y = (int)(height - (value / this.upper_y_value) * height);
-        
-        g.setColor(color);
-        
-        if (this.isStandardDeviationPainted && this.measureSelected <= 6) {
-            // access the corresponding std value
-            double std = m.getLastValue(this.measureSelected + 7);
-            int len = (int) ((std/this.upper_y_value)*height);
+
+        g.setColor(this.colors[i]);
+
+        if (this.isStandardDeviationPainted) {
+            int len = (int) ((this.measureStds[i].getLastValue(this.measureSelected)/this.upper_y_value)*height);
             paintStandardDeviation(g, len, x, y);
         }
         
@@ -111,25 +98,5 @@ public class GraphScatter extends AbstractGraphPlot {
     	
     }
     
-    private void scatterCurrentParam(Graphics g, MeasureCollection m, Color color) {
-        
-        int height = getHeight();
-        int width = getWidth();
-        
-        int x = (int)(((m.getLastValue(6) - this.lower_x_value) / (this.upper_x_value - this.lower_x_value)) * width); 
-        double value = m.getLastValue(this.measureSelected);  
-
-        if(Double.isNaN(value)){
-            // no result for this budget yet
-            return;
-        }
-        
-        int y = (int)(height - (value / this.upper_y_value) * height);
-        
-        g.setColor(color);
-        g.fillOval(x - DOT_SIZE/2, y - DOT_SIZE/2, DOT_SIZE, DOT_SIZE);
-        
-    }
-
 }
  

moa/src/main/java/moa/evaluation/ALMeasureCollection.java
@@ -37,10 +37,8 @@ public class ALMeasureCollection extends MeasureCollection implements Classifica
 
     @Override
     protected String[] getNames() {
-        return new String[]{"Accuracy", "Kappa", "Kappa Temp", "Ram-Hours", 
-                    "Time", "Memory", "Label Acq. Rate", "Accuracy [std]", 
-                    "Kappa [std]", "Kappa Temp [std]", "Ram-Hours [std]",
-                    "Time [std]", "Memory [std]", "Label Acq. Rate [std]"};
+        return new String[]{"Accuracy", "Kappa", "Kappa Temp", "Ram-Hours",
+                    "Time", "Memory", "Label Acq. Rate"};
     }
 
     @Override

moa/src/main/java/moa/gui/active/ALTaskTextViewerPanel.java
@@ -106,10 +106,6 @@ public class ALTaskTextViewerPanel extends JPanel {
 	
 	private JPanel panelEvalOutput;
 	
-	private MeasureCollection[] measuresAll;
-	
-	private MeasureCollection[] measuresSpecial;
-	
 	private MeasureOverview measureOverview;
 	
 	private GridBagConstraints gridBagConstraints;
@@ -148,7 +144,7 @@ public class ALTaskTextViewerPanel extends JPanel {
 	
 	private double[] variedParamValues;
 	
-	private Color[] colors;
+	private double[] budgets;
 
 	public ALTaskTextViewerPanel() {
 
@@ -253,11 +249,12 @@ public class ALTaskTextViewerPanel extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int selected = Integer.parseInt(e.getActionCommand());
-                graphCanvas.setGraph(measuresSpecial, selected, graphCanvas.getProcessFrequencies(),
-                        graphCanvas.getMinProcessFrequency(), colors);
-                paramGraphCanvas.setGraph(measuresSpecial, selected, 
-                        variedParamValues, colors);
-                budgetGraphCanvas.setGraph(measuresAll, selected, variedParamValues, colors);
+                graphCanvas.setMeasureSelected(selected);
+                graphCanvas.updateCanvas(true);
+                paramGraphCanvas.setMeasureSelected(selected);
+                paramGraphCanvas.updateCanvas(true);
+                budgetGraphCanvas.setMeasureSelected(selected);
+                budgetGraphCanvas.updateCanvas(true);
             }
         });
 
@@ -349,7 +346,7 @@ public class ALTaskTextViewerPanel extends JPanel {
 		// graphCanvas displays the live graph
 		graphCanvas = new ProcessGraphCanvas();
 		graphCanvas.setPreferredSize(new Dimension(500, 111));
-		graphCanvas.setGraph(null, 0, null, 1000, null);
+		graphCanvas.setGraph(null, null, null, 1000, null);
 
 		GroupLayout graphCanvasLayout = new GroupLayout(graphCanvas);
 		graphCanvas.setLayout(graphCanvasLayout);
@@ -365,9 +362,9 @@ public class ALTaskTextViewerPanel extends JPanel {
 		paramGraphScrollPanel = new JScrollPane();
 
 		// paramGraphCanvas displays the live varied parameter graph
-		paramGraphCanvas = new ParamGraphCanvas(false);
+		paramGraphCanvas = new ParamGraphCanvas();
 		paramGraphCanvas.setPreferredSize(new Dimension(500, 111));
-		paramGraphCanvas.setGraph(null, 0, null, null);
+		paramGraphCanvas.setGraph(null, null, null, null);
 
 		GroupLayout paramGraphCanvasLayout = new GroupLayout(paramGraphCanvas);
 		paramGraphCanvas.setLayout(paramGraphCanvasLayout);
@@ -383,9 +380,9 @@ public class ALTaskTextViewerPanel extends JPanel {
         budgetGraphScrollPanel = new JScrollPane();
 		
 		// budgetGraphCanvas displays the live actually used budget graph
-		budgetGraphCanvas = new ParamGraphCanvas(true);
+		budgetGraphCanvas = new ParamGraphCanvas();
 		budgetGraphCanvas.setPreferredSize(new Dimension(500, 111));
-        budgetGraphCanvas.setGraph(null, 0, null, null);
+        budgetGraphCanvas.setGraph(null, null, null, null);
         
         GroupLayout budgetGraphCanvasLayout = new GroupLayout(budgetGraphCanvas);
         budgetGraphCanvas.setLayout(budgetGraphCanvasLayout);
@@ -571,17 +568,15 @@ public class ALTaskTextViewerPanel extends JPanel {
 	public void setGraph(Preview preview, Color[] colors) {
 		if (preview == null) {
 			// no preview received
-			this.measuresAll = null;
-		    this.measuresSpecial = null;
 		    this.measureOverview.update(null, "", null);
-			this.graphCanvas.setGraph(null, 0, null, 1000, null);
-			this.paramGraphCanvas.setGraph(null, 0, null, null);
-			this.budgetGraphCanvas.setGraph(null, 0, null, null);
+			this.graphCanvas.setGraph(null, null, null, 1000, null);
+			this.paramGraphCanvas.setGraph(null, null, null, null);
+			this.budgetGraphCanvas.setGraph(null, null, null, null);
 			return;
 		}
 
-		GraphCanvasMultiParams gcmpAll = new GraphCanvasMultiParams();
-		GraphCanvasMultiParams gcmpSpecial = new GraphCanvasMultiParams();
+		ParsedPreview pp;
+		ParsedPreview ppStd;
 		
 		// check which type of task it is
 		Class<?> c = preview.getTaskClass();
@@ -593,23 +588,18 @@ public class ALTaskTextViewerPanel extends JPanel {
 			this.variedParamName = pc.getVariedParamName();
             this.variedParamValues = pc.getVariedParamValues();
 
-			// read all previews
-			gcmpAll = readPreviewCollection(pc, false);
-			
 			if (c == ALPartitionEvaluationTask.class) {
 				// calculate mean preview collection for each parameter value
 				MeanPreviewCollection mpc = new MeanPreviewCollection(
 						(PreviewCollection<PreviewCollection<Preview>>) preview);
-				pc = mpc.getMeanPreviews();
-				pc = mpc.getStdPreviews();
+				pp = readCollection(mpc.getMeanPreviews());
+				ppStd = readCollection(mpc.getStdPreviews());
 				
-				// TODO: handle separate previews
-				
-				gcmpSpecial = readPreviewCollection(pc, true);
 				this.graphCanvas.setStandardDeviationPainted(true);
 				this.paramGraphCanvas.setStandardDeviationPainted(true);
 			} else {
-			    gcmpSpecial = readPreviewCollection(pc, false);
+				pp = readCollection(pc);
+				ppStd = null;
 			    this.graphCanvas.setStandardDeviationPainted(false);
 			    this.paramGraphCanvas.setStandardDeviationPainted(false);
 			}
@@ -625,8 +615,8 @@ public class ALTaskTextViewerPanel extends JPanel {
     		
     	} else if (c == ALPrequentialEvaluationTask.class) {
     		// read Preview (in this case, no special preview is required)
-    		gcmpAll = readPreview(preview, false);
-    		gcmpSpecial = gcmpAll;
+    		pp = read(preview);
+    		ppStd = null;
     		
     		// reset varied param name and values
     		this.variedParamName = "";
@@ -651,33 +641,33 @@ public class ALTaskTextViewerPanel extends JPanel {
     		
     	} else {
     		// sth went wrong
-    		this.measuresAll = null;
-    	    this.measuresSpecial = null;
     	    this.measureOverview.update(null, "", null);
-    		this.graphCanvas.setGraph(null, 0, null, 1000, null);
-    		this.paramGraphCanvas.setGraph(null, 0, null, null);
-    		this.budgetGraphCanvas.setGraph(null, 0, null, null);
+    		this.graphCanvas.setGraph(null, null, null, 1000, null);
+    		this.paramGraphCanvas.setGraph(null, null, null, null);
+    		this.budgetGraphCanvas.setGraph(null, null, null, null);
 			return;
     	}
-		
-		this.measuresAll = gcmpAll.getMeasureCollectionsArray();
-		
-		int[] pfsSpecial = gcmpSpecial.getProcessFrequenciesArray();
-		this.measuresSpecial = gcmpSpecial.getMeasureCollectionsArray();
-		int min_pf = min(pfsSpecial);
-		
-		this.colors = colors;
-		
-		this.measureOverview.update(this.measuresSpecial, this.variedParamName, this.variedParamValues);
-		this.graphCanvas.setGraph(this.measuresSpecial, 
-				this.graphCanvas.getMeasureSelected(), pfsSpecial, min_pf, colors);
-		this.paramGraphCanvas.setGraph(this.measuresSpecial, 
-				this.paramGraphCanvas.getMeasureSelected(), 
-				this.variedParamValues, colors);
-		this.budgetGraphCanvas.setGraph(
-				this.measuresAll, 
-				this.budgetGraphCanvas.getMeasureSelected(), 
-				this.variedParamValues, colors);
+
+		MeasureCollection[] measures = pp.getMeasureCollectionsArray();
+		MeasureCollection[] measuresStd = null;
+		if (ppStd != null) {
+			measuresStd = ppStd.getMeasureCollectionsArray();
+		}
+
+		// restructure latest budgets to make them readable for the
+		// GraphScatter class
+		this.budgets = new double[measures.length];
+		for (int i = 0; i < measures.length; i++) {
+			this.budgets[i] = measures[i].getLastValue(6);
+		}
+
+		int[] pfs = pp.getProcessFrequenciesArray();
+		int min_pf = min(pfs);
+
+		this.measureOverview.update(measures, this.variedParamName, this.variedParamValues);
+		this.graphCanvas.setGraph(measures, measuresStd, pfs, min_pf, colors);
+		this.paramGraphCanvas.setGraph(measures, measuresStd, this.variedParamValues, colors);
+		this.budgetGraphCanvas.setGraph(measures, measuresStd, this.budgets, colors);
 	}
 	
 	private static int min(int[] l) {
@@ -696,14 +686,14 @@ public class ALTaskTextViewerPanel extends JPanel {
 	
 	/**
 	 * Parses a PreviewCollection and return the resulting 
-	 * GraphCanvasMultiParams. If the PreviewCollection contains
+	 * ParsedPreview object. If the PreviewCollection contains
 	 * PreviewCollections again, it recursively adds their results. If it
 	 * contains simple Previews, it adds their properties to the result.
 	 * @param pc PreviewCollection
 	 * @return relevant information contained in the PreviewCollection
 	 */
-	public GraphCanvasMultiParams readPreviewCollection(PreviewCollection<Preview> pc, boolean withStd) {	
-		GraphCanvasMultiParams gcmp = new GraphCanvasMultiParams();
+	public ParsedPreview readCollection(PreviewCollection<Preview> pc) {	
+		ParsedPreview pp = new ParsedPreview();
 		List<Preview> sps = pc.getPreviews();
 
 		if (sps.size() > 0 && sps.get(0) instanceof PreviewCollection) {
@@ -711,36 +701,36 @@ public class ALTaskTextViewerPanel extends JPanel {
 			// NOTE: this assumes that all elements in sps are of the same type
 			for (Preview sp: sps) {
 				@SuppressWarnings("unchecked")
-				GraphCanvasMultiParams tmp = readPreviewCollection((PreviewCollection<Preview>) sp, withStd);
-				gcmp.add(tmp);
+				ParsedPreview tmp = readCollection((PreviewCollection<Preview>) sp);
+				pp.add(tmp);
 			}
 		} else {
 			// members are simple previews
 			for (Preview sp: sps) {
-				GraphCanvasMultiParams tmp = readPreview(sp, withStd);
-				gcmp.add(tmp);
+				ParsedPreview tmp = read(sp);
+				pp.add(tmp);
 			}
 		}
-		
-		return gcmp;
+
+		return pp;
 	}
 	
 	/**
-	 * GraphCanvasMultiParams represents the parsing results of a preview,
+	 * ParsedPreview represents the parsing results of a preview,
 	 * namely the process frequencies and the measure collections.
 	 * @author Tim Sabsch (tim.sabsch@ovgu.de)
 	 * @version $Revision: 1 $
 	 */
-	private class GraphCanvasMultiParams {
+	private class ParsedPreview {
 		private List<Integer> processFrequencies;
 		private List<MeasureCollection> measureCollections;
-		
-		public GraphCanvasMultiParams() {
+
+		public ParsedPreview() {
 			this.processFrequencies = new ArrayList<Integer>();
 			this.measureCollections = new ArrayList<MeasureCollection>();
 		}
 		
-		public void add(GraphCanvasMultiParams other) {
+		public void add(ParsedPreview other) {
 			this.processFrequencies.addAll(other.getProcessFrequencies());
 			this.measureCollections.addAll(other.getMeasureCollections());
 		}
@@ -774,7 +764,7 @@ public class ALTaskTextViewerPanel extends JPanel {
 	 * measurements.
 	 * @param preview
 	 */
-	private GraphCanvasMultiParams readPreview(Preview p, boolean withStd) {
+	private ParsedPreview read(Preview p) {
 		
 		// find measure columns
 		String[] measureNames = p.getMeasurementNames();
@@ -788,14 +778,6 @@ public class ALTaskTextViewerPanel extends JPanel {
 		int timeColumn = -1;
 		int memoryColumn = -1;
 		int budgetColumn = -1;
-		
-        int accuracyColumnStd = -1;
-        int kappaColumnStd = -1;
-        int kappaTempColumnStd = -1;
-        int ramColumnStd = -1;
-        int timeColumnStd = -1;
-        int memoryColumnStd = -1;
-        int budgetColumnStd = -1;
 
 		for (int i = 0; i < numMeasures; i++) {
 			switch (measureNames[i]) {
@@ -804,65 +786,47 @@ public class ALTaskTextViewerPanel extends JPanel {
 				break;
 			case "classifications correct (percent)":
 			case "[avg] classifications correct (percent)":
+			case "[std] classifications correct (percent)":
 				accuracyColumn = i; 
 				break;
 			case "Kappa Statistic (percent)":
 			case "[avg] Kappa Statistic (percent)":
+			case "[std] Kappa Statistic (percent)":
 				kappaColumn = i;
 				break;
 			case "Kappa Temporal Statistic (percent)":
 			case "[avg] Kappa Temporal Statistic (percent)":
+			case "[std] Kappa Temporal Statistic (percent)":
 				kappaTempColumn = i;
 				break;
 			case "model cost (RAM-Hours)":
+			case "[std] model cost (RAM-Hours)":
 				ramColumn = i;
 				break;
 			case "evaluation time (cpu seconds)":
 			case "total train time":
+			case "[std] evaluation time (cpu seconds)":
 				timeColumn = i;
 				break;
 			case "model serialized size (bytes)":
+			case "[std] model serialized size (bytes)":
 				memoryColumn = i;
 				break;
 			case "Rel Number of Label Acquisitions":
+			case "[std] Rel Number of Label Acquisitions":
 				budgetColumn = i;
 				break;
-
-            case "[std] classifications correct (percent)":
-            case "[std] [avg] classifications correct (percent)":
-                accuracyColumnStd = i; 
-                break;
-            case "[std] Kappa Statistic (percent)":
-            case "[std] [avg] Kappa Statistic (percent)":
-                kappaColumnStd = i;
-                break;
-            case "[std] Kappa Temporal Statistic (percent)":
-            case "[std] [avg] Kappa Temporal Statistic (percent)":
-                kappaTempColumnStd = i;
-                break;
-            case "[std] model cost (RAM-Hours)":
-                ramColumnStd = i;
-                break;
-            case "[std] evaluation time (cpu seconds)":
-            case "[std] total train time":
-                timeColumnStd = i;
-                break;
-            case "[std] model serialized size (bytes)":
-                memoryColumnStd = i;
-                break;
-            case "[std] Rel Number of Label Acquisitions":
-                budgetColumnStd = i;
-                break;
 			default:
 				break;
 			}
 		}
-		
+
 		List<double[]> data = p.getData();
-		
+
 		MeasureCollection m = new ALMeasureCollection();
-    		
+
 		// set entries
+		// TODO: obviously this should be changed into a dict
 		for (double[] entry: data) {
 			m.addValue(0, entry[accuracyColumn]);
 			m.addValue(1, entry[kappaColumn]);
@@ -871,24 +835,14 @@ public class ALTaskTextViewerPanel extends JPanel {
 			m.addValue(4, entry[timeColumn]);
 			m.addValue(5, entry[memoryColumn] / (1024 * 1024));
 			m.addValue(6, entry[budgetColumn]);
-			
-			if (withStd) {
-                m.addValue(7, entry[accuracyColumnStd]);
-                m.addValue(8, entry[kappaColumnStd]);
-                m.addValue(9, entry[kappaTempColumnStd]);
-                m.addValue(10, Math.abs(entry[ramColumnStd]));
-                m.addValue(11, entry[timeColumnStd]);
-                m.addValue(12, entry[memoryColumnStd] / (1024 * 1024));
-                m.addValue(13, entry[budgetColumnStd]);
-			}
     	}
-		
+
 		// determine process frequency
 		int processFrequency = (int) data.get(0)[processFrequencyColumn];
-		
-		GraphCanvasMultiParams gcmp = new GraphCanvasMultiParams();
-		gcmp.addMeasureCollection(m);
-		gcmp.addProcessFrequency(processFrequency);
-		return gcmp;
+
+		ParsedPreview pp = new ParsedPreview();
+		pp.addMeasureCollection(m);
+		pp.addProcessFrequency(processFrequency);
+		return pp;
 	}
 }

moa/src/main/java/moa/gui/visualization/AbstractGraphCanvas.java
@@ -48,6 +48,8 @@ public abstract class AbstractGraphCanvas extends JPanel {
 
     protected MeasureCollection[] measures;
 
+    protected MeasureCollection[] measureStds;
+    
     protected int measureSelected;
 
     protected AbstractGraphAxes axesPanel;
@@ -81,13 +83,13 @@ public abstract class AbstractGraphCanvas extends JPanel {
         this.axesPanel = ax;
         this.plotPanel = g;
 
+        this.measureSelected = 0;
+
         this.plotPanel.setLocation(X_OFFSET_LEFT + 1, Y_OFFSET_TOP);
 
         add(this.axesPanel);
         this.axesPanel.add(this.plotPanel);
 
-        this.measureSelected = 0;
-        
         this.min_x_value = 0;
         this.max_x_value = 1;
         this.max_y_value = 1;
@@ -167,12 +169,14 @@ public abstract class AbstractGraphCanvas extends JPanel {
     }
 
     /**
-     * Returns the currently selected measure index.
-     * 
-     * @return currently selected measure index
+     * Sets the currently selected measure index.
+     *
+     * @param selected
+     * 			  new selected measure
      */
-    public int getMeasureSelected() {
-        return this.measureSelected;
+    public void setMeasureSelected(int selected) {
+    	this.measureSelected = selected;
+        this.plotPanel.setMeasureSelected(selected);
     }
 
     /**

moa/src/main/java/moa/gui/visualization/AbstractGraphPlot.java
@@ -41,6 +41,7 @@ public abstract class AbstractGraphPlot extends JPanel{
 	private static final long serialVersionUID = 1L;
 	
 	protected MeasureCollection[] measures;
+	protected MeasureCollection[] measureStds;
     protected int measureSelected;
 	
     protected double min_x_value;
@@ -81,21 +82,31 @@ public abstract class AbstractGraphPlot extends JPanel{
             .addGap(0, 300, Short.MAX_VALUE)
         );
 	}
-	
+
 	/**
 	 * Sets the graph by updating the measures and currently measure index.
 	 * This method should not be directly called, but may be used by subclasses
 	 * to save space.
 	 * @param measures measure information
-	 * @param mSelect index of the currently selected measure
+	 * @param stds standard deviation of the measures
 	 * @param colors color encoding for the plots
 	 */
-	protected void setGraph(MeasureCollection[] measures, int mSelect, Color[] colors) {
+	protected void setGraph(MeasureCollection[] measures, MeasureCollection[] stds, Color[] colors) {
 		this.measures = measures;
-	    this.measureSelected = mSelect;
+		this.measureStds = stds;
 	    this.colors = colors;
 	    repaint();
 	}
+
+    /**
+     * Sets the currently selected measure index.
+     *
+     * @param selected
+     * 			  new selected measure
+     */
+    public void setMeasureSelected(int selected) {
+        this.measureSelected = selected;
+    }
 
     /**
      * Sets minimum x value.
@@ -167,7 +178,6 @@ public abstract class AbstractGraphPlot extends JPanel{
         this.isStandardDeviationPainted = b;
     }
 
-    
     protected void paintStandardDeviation(Graphics g, int len, int x, int y) {
         // draw vertical line
         g.drawLine(x, y-len, x, y+len);

moa/src/main/java/moa/gui/visualization/GraphMultiCurve.java
@@ -41,13 +41,14 @@ public class GraphMultiCurve extends AbstractGraphPlot {
     /**
      * Updates the measure collection information and repaints the curves.
      * @param measures 			 information about the curves
-     * @param mSelect 		 	 currently selected measure
+     * @param measureStds		 standard deviation of the measures
      * @param processFrequencies information about the process frequencies of
      * 							 the curves
+     * @param colors			 color encodings of the curves
      */
-    protected void setGraph(MeasureCollection[] measures, int mSelect, int[] processFrequencies, Color[] colors){
-    	this.processFrequencies = processFrequencies;   
-    	super.setGraph(measures, mSelect, colors);
+    protected void setGraph(MeasureCollection[] measures, MeasureCollection[] measureStds, int[] processFrequencies, Color[] colors){
+//    	this.processFrequencies = processFrequencies;   
+    	super.setGraph(measures, measureStds, colors);
     }
     
     /**
@@ -63,54 +64,49 @@ public class GraphMultiCurve extends AbstractGraphPlot {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
+
         if (this.measures == null) { 
         	// no measures received yet -> nothing to paint
         	return; 
         }
-        
+
         // paint all curves
         for (int i = 0; i < this.measures.length; i++) {
-        	paintFullCurve(g, this.measures[i], this.measureSelected, this.processFrequencies[i], this.colors[i]);
+        	paintFullCurve(g, i);
         }
-        
+
     }
 
     /**
      * Draws a single curve on the canvas.
      * @param g 	  the Graphics context in which to paint
-     * @param m 	  curve information
-     * @param mSelect currently selected measure
-     * @param pf 	  process frequency of the curve
-     * @param color   colour the curve will be drawn in
+	 * @param i       index of the currently selected measure
      */
-    private void paintFullCurve(Graphics g, MeasureCollection m, int mSelect, int pf, Color color){
-            if (m.getNumberOfValues(mSelect) == 0) {
+    private void paintFullCurve(Graphics g, int i){
+            if (this.measures[i].getNumberOfValues(this.measureSelected) == 0) {
             	// no values of this measure available
             	return;
             }
-            
-            g.setColor(color);
-            
+
+            g.setColor(this.colors[i]);
+
             int height = getHeight();
-            
+
 //          // compute the relation of minimum PF and current PF
 //          double processFrequencyFactor = pf / this.min_processFrequency;
 
-            int n = m.getNumberOfValues(mSelect);
+            int n = this.measures[i].getNumberOfValues(this.measureSelected);
 
             int[] x = new int[n];
             int[] y = new int[n];
 
-            for (int i = 0; i < n; i ++) {
-            	x[i] = (int) (i * x_resolution);
-            	y[i] = (int)(height-(m.getValue(mSelect, i)/this.upper_y_value)*height);
-            	
-            	if (this.isStandardDeviationPainted && mSelect <= 6) {
-            	    // access the corresponding std value
-            	    double std = m.getValue(mSelect + 7, i);
-            	    int len = (int) ((std/this.upper_y_value)*height);
-            	    paintStandardDeviation(g, len, x[i], y[i]);
+            for (int j = 0; j < n; j++) {
+            	x[j] = (int) (j * x_resolution);
+            	y[j] = (int)(height-(this.measures[i].getValue(this.measureSelected, j)/this.upper_y_value)*height);
+
+            	if (this.isStandardDeviationPainted) {
+            	    int len = (int) ((this.measureStds[i].getValue(this.measureSelected, j)/this.upper_y_value)*height);
+            	    paintStandardDeviation(g, len, x[j], y[j]);
             	}
             }
 

moa/src/main/java/moa/gui/visualization/ParamGraphCanvas.java
@@ -43,28 +43,27 @@ public class ParamGraphCanvas extends AbstractGraphCanvas {
      * ParamGraphAxes as instance of AbstractGraphAxes and GraphScatter as
      * instance of AbstractGraphPlot.
      */
-    public ParamGraphCanvas(boolean paintCurrentParam) {
-        super(new ParamGraphAxes(), new GraphScatter(paintCurrentParam));
+    public ParamGraphCanvas() {
+        super(new ParamGraphAxes(), new GraphScatter());
     }
 
     /**
      * Sets the scatter graph.
-     * 
+     *
      * @param measures
      *            information about the curves
-     * @param mSelect
-     *            currently selected measure
+     * @param measureStds
+     *            standard deviation for the measures
      * @param variedParamValues
      *            values of the varied parameter
      * @param colors
      *            color encoding for the param array
      */
-    public void setGraph(MeasureCollection[] measures, int mSelect,
+    public void setGraph(MeasureCollection[] measures, MeasureCollection[] measureStds,
             double[] variedParamValues, Color[] colors) {
         this.measures = measures;
-        this.measureSelected = mSelect;
         this.variedParamValues = variedParamValues;
-        ((GraphScatter) this.plotPanel).setGraph(measures, mSelect,
+        ((GraphScatter) this.plotPanel).setGraph(measures, measureStds,
                 variedParamValues, colors);
         updateCanvas(false);
     }

commit 59353ad650986342d6372fffec3e7c937ae4d27d

Fix missing y value computation
Commit 5ac991ea accidentally removed the computation of the y value in GraphScatter.

moa/src/main/java/moa/gui/visualization/GraphScatter.java
@@ -86,6 +86,7 @@ public class GraphScatter extends AbstractGraphPlot {
             return;
         }
 
+        int y = (int)(height - (value / this.upper_y_value) * height);
 
         g.setColor(this.colors[i]);
 