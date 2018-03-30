573f92e63019e5173385ddc861ed7c6c148547d7
Peter Reutemann <fracpete@gmail.com>
2018-03-30 17:03:03 +1300

    added detection of base directory to allow launching of scripts from anywhere

--- moa/src/main/scripts/moa.sh
+++ moa/src/main/scripts/moa.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BASEDIR=`dirname $0`/..
+BASEDIR=`(cd "$BASEDIR"; pwd)`
 MEMORY=512m
-java -Xmx$MEMORY -cp "../lib/moa-${project.version}:../lib/*" -javaagent:../lib/sizeofag-1.0.2.jar moa.gui.GUI
+java -Xmx$MEMORY -cp "$BASEDIR/lib/moa-${project.version}:$BASEDIR/lib/*" -javaagent:$BASEDIR/lib/sizeofag-1.0.2.jar moa.gui.GUI
 
