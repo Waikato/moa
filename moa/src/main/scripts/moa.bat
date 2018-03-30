86c4a9f8d9e053b5c663c35ba3d4f5c8d9fe87f1
Peter Reutemann <fracpete@gmail.com>
2018-03-30 16:59:24 +1300

    added filtering to assembly to allow moving moa jar to front of classpath

--- moa/src/main/scripts/moa.bat
+++ moa/src/main/scripts/moa.bat
@@ -2,5 +2,5 @@
 
 set MEMORY=512m
 
-java -Xmx%MEMORY% -cp "../lib/*" -javaagent:../lib/sizeofag-1.0.2.jar moa.gui.GUI
+java -Xmx%MEMORY% -cp "../lib/moa-${project.version}../lib/*" -javaagent:../lib/sizeofag-1.0.2.jar moa.gui.GUI
 
