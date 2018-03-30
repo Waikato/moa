2f01eb513713547ffc4a49d97b000425e173570a
Peter Reutemann <fracpete@gmail.com>
2018-03-30 15:36:53 +1300

    updated scripts to use new sizeofag.jar

--- moa/src/main/scripts/moa.sh
+++ moa/src/main/scripts/moa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 MEMORY=512m
-java -Xmx$MEMORY -cp "../lib/*" -javaagent:../lib/sizeofag-1.0.0.jar moa.gui.GUI
+java -Xmx$MEMORY -cp "../lib/*" -javaagent:../lib/sizeofag-1.0.2.jar moa.gui.GUI
 
