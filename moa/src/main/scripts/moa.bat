f417c0c8e6aa0a090fffa41b0e754c2e611cbb0b
Peter Reutemann <fracpete@gmail.com>
2018-04-02 21:30:28 +1200

    updated assembly to put platform-specific netlib libraries in sub-dirs; updated shell scripts to include these sub-dirs depending on actual platform

--- moa/src/main/scripts/moa.bat
+++ moa/src/main/scripts/moa.bat
@@ -2,5 +2,10 @@
 
 set BASEDIR=%~dp0\..
 set MEMORY=512m
-java -Xmx%MEMORY% -cp "%BASEDIR%/lib/moa-${project.version}../lib/*" -javaagent:%BASEDIR%/lib/sizeofag-1.0.2.jar moa.gui.GUI
+IF "%PROCESSOR_ARCHITECTURE%"=="x86" (set ARCH=32BIT) else (set ARCH=64BIT)
+
+if "%ARCH%" == "32BIT" set ARCHLIB=%BASEDIR%/windows32/*
+if "%ARCH%" == "64BIT" set ARCHLIB=%BASEDIR%/windows64/*
+
+java -Xmx%MEMORY% -cp "%BASEDIR%/lib/moa-${project.version};%BASEDIR%/lib/*;%ARCHLIB%" -javaagent:%BASEDIR%/lib/sizeofag-1.0.2.jar moa.gui.GUI
 
--- moa/src/main/scripts/moa.sh
+++ moa/src/main/scripts/moa.sh
@@ -3,5 +3,16 @@
 BASEDIR=`dirname $0`/..
 BASEDIR=`(cd "$BASEDIR"; pwd)`
 MEMORY=512m
-java -Xmx$MEMORY -cp "$BASEDIR/lib/moa-${project.version}:$BASEDIR/lib/*" -javaagent:$BASEDIR/lib/sizeofag-1.0.2.jar moa.gui.GUI
+case "`uname`" in
+  Darwin*) ARCHLIB="$BASEDIR/lib/macosx64/*"
+    ;;
+  *)
+    if [ `uname -m` == "i686" ]
+    then
+      ARCHLIB="$BASEDIR/lib/linux32/*"
+    else
+      ARCHLIB="$BASEDIR/lib/linux64/*"
+    fi
+esac
+java -Xmx$MEMORY -cp "$BASEDIR/lib/moa-${project.version}:$BASEDIR/lib/*:$ARCHLIB" -javaagent:$BASEDIR/lib/sizeofag-1.0.2.jar moa.gui.GUI
 
