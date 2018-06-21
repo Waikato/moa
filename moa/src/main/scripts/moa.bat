e5832af6fd989556f5c99e4538084bf4b4c8e1b2
Peter Reutemann <fracpete@gmail.com>
2018-06-22 09:10:05 +1200

    netlib libraries don't have to be in separate sub-dirs, which simplifies scripts

--- moa/src/main/scripts/moa.bat
+++ moa/src/main/scripts/moa.bat
@@ -2,10 +2,6 @@
 
 set BASEDIR=%~dp0\..
 set MEMORY=512m
-IF "%PROCESSOR_ARCHITECTURE%"=="x86" (set ARCH=32BIT) else (set ARCH=64BIT)
 
-if "%ARCH%" == "32BIT" set ARCHLIB=%BASEDIR%/windows32/*
-if "%ARCH%" == "64BIT" set ARCHLIB=%BASEDIR%/windows64/*
-
-java -Xmx%MEMORY% -cp "%BASEDIR%/lib/moa-${project.version};%BASEDIR%/lib/*;%ARCHLIB%" -javaagent:%BASEDIR%/lib/sizeofag-1.0.2.jar moa.gui.GUI
+java -Xmx%MEMORY% -cp "%BASEDIR%/lib/moa-${project.version};%BASEDIR%/lib/*" -javaagent:%BASEDIR%/lib/sizeofag-1.0.2.jar moa.gui.GUI
 
--- moa/src/main/scripts/moa.sh
+++ moa/src/main/scripts/moa.sh
@@ -3,16 +3,5 @@
 BASEDIR=`dirname $0`/..
 BASEDIR=`(cd "$BASEDIR"; pwd)`
 MEMORY=512m
-case "`uname`" in
-  Darwin*) ARCHLIB="$BASEDIR/lib/macosx64/*"
-    ;;
-  *)
-    if [ `uname -m` == "i686" ]
-    then
-      ARCHLIB="$BASEDIR/lib/linux32/*"
-    else
-      ARCHLIB="$BASEDIR/lib/linux64/*"
-    fi
-esac
-java -Xmx$MEMORY -cp "$BASEDIR/lib/moa-${project.version}:$BASEDIR/lib/*:$ARCHLIB" -javaagent:$BASEDIR/lib/sizeofag-1.0.2.jar moa.gui.GUI
+java -Xmx$MEMORY -cp "$BASEDIR/lib/moa-${project.version}:$BASEDIR/lib/*" -javaagent:$BASEDIR/lib/sizeofag-1.0.2.jar moa.gui.GUI
 
