ee7523c6f1aff7e62bbae905a53638150d1ef197
Peter Reutemann <fracpete@gmail.com>
2018-06-30 14:51:40 +1200

    updated sizeofag version

--- moa/src/main/scripts/moa.sh
+++ moa/src/main/scripts/moa.sh
@@ -3,5 +3,5 @@
 BASEDIR=`dirname $0`/..
 BASEDIR=`(cd "$BASEDIR"; pwd)`
 MEMORY=512m
-java -Xmx$MEMORY -cp "$BASEDIR/lib/moa-${project.version}:$BASEDIR/lib/*" -javaagent:$BASEDIR/lib/sizeofag-1.0.2.jar moa.gui.GUI
+java -Xmx$MEMORY -cp "$BASEDIR/lib/moa-${project.version}:$BASEDIR/lib/*" -javaagent:$BASEDIR/lib/sizeofag-1.0.4.jar moa.gui.GUI
 
