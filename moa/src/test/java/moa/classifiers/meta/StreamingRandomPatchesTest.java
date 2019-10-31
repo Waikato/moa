363a16bdac5e3fc681830d5c75f36c9d01745c17
hmgomes <heitor.gomes@waikato.ac.nz>
2019-10-31 17:41:23 +1300

    Adding the test resources for StreamingRandomPatches

---
+++ moa/src/test/java/moa/classifiers/meta/StreamingRandomPatchesTest.java
@@ -0,0 +1,81 @@
+/*
+ *   StreamingRandomPatchesTest.java
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 
+ */
+package moa.classifiers.meta;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import moa.classifiers.AbstractMultipleClassifierTestCase;
+import moa.classifiers.Classifier;
+
+/**
+ * Tests the StreamingRandomPatches classifier.
+ * 
+ * @author  Heitor Murilo Gomes (heitor dot gomes at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class StreamingRandomPatchesTest
+  extends AbstractMultipleClassifierTestCase {
+
+  /**
+   * Constructs the test case. Called by subclasses.
+   *
+   * @param name 	the name of the test
+   */
+  public StreamingRandomPatchesTest(String name) {
+    super(name);
+    this.setNumberTests(1);
+  }
+
+  /**
+   * Returns the classifier setups to use in the regression test.
+   *
+   * @return		the setups
+   */
+  @Override
+  protected Classifier[] getRegressionClassifierSetups() {
+    StreamingRandomPatches SRPTest = new StreamingRandomPatches();
+    SRPTest.ensembleSizeOption.setValue(5);
+    SRPTest.subspaceModeOption.setChosenIndex(0);
+    SRPTest.subspaceSizeOption.setValue(2);
+
+    return new Classifier[]{
+            SRPTest,
+    };
+  }
+  
+  /**
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(StreamingRandomPatchesTest.class);
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    runTest(suite());
+  }
+}
