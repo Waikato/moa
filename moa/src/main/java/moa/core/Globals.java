5f02ba9ed37c4086124c71973bb179ff440131f8
csterling <coreytsterling@gmail.com>
2020-12-15 14:57:29 +1300

    Pre-release for version 2020.12.0.

--- moa/src/main/java/moa/core/Globals.java
+++ moa/src/main/java/moa/core/Globals.java
@@ -29,7 +29,7 @@ public class Globals {
 
     public static final String workbenchTitle = "{M}assive {O}nline {A}nalysis";
 
-    public static final String versionString = " 20.07 July 2020";
+    public static final String versionString = " 20.12 December 2020";
 
     public static final String copyrightNotice = "(C) 2007-2020 University of Waikato, Hamilton, New Zealand";
 
