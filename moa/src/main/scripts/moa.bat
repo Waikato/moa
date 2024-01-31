2ff8dcf719c706df0e955663d2f91e9940d2052f
Peter Reutemann <fracpete@gmail.com>
2024-02-01 10:39:30 +1300

    upgraded sizeofag to 1.1.0

--- moa/src/main/scripts/moa.bat
+++ moa/src/main/scripts/moa.bat
@@ -3,5 +3,5 @@
 set BASEDIR=%~dp0\..
 set MEMORY=512m
 
-java -Xmx%MEMORY% -cp "%BASEDIR%/lib/*" -javaagent:"%BASEDIR%/lib/sizeofag-1.0.4.jar" moa.gui.GUI
+java -Xmx%MEMORY% -cp "%BASEDIR%/lib/*" -javaagent:"%BASEDIR%/lib/sizeofag-1.1.0.jar" moa.gui.GUI
 
