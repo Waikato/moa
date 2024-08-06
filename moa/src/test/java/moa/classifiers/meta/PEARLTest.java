238eb65c421f7da532253c9d25202bac0f0f33a2
nuwangunasekara <ng198002@gmail.com>
2024-08-07 07:38:18 +0900

    Add PEARL tests

---
+++ moa/src/test/java/moa/classifiers/meta/PEARLTest.java
@@ -0,0 +1,81 @@
+/*
+ *   AdaptiveRandomForestTest.java
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 
+ */
+package moa.classifiers.meta;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import moa.classifiers.AbstractMultipleClassifierTestCase;
+import moa.classifiers.Classifier;
+
+/**
+ * Tests the PEARL classifier.
+ * 
+ * @author  Nuwan Gunasekara (ng98 at students dot waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class PEARLTest
+  extends AbstractMultipleClassifierTestCase {
+
+  /**
+   * Constructs the test case. Called by subclasses.
+   *
+   * @param name 	the name of the test
+   */
+  public PEARLTest(String name) {
+    super(name);
+    this.setNumberTests(1);
+  }
+
+  /**
+   * Returns the classifier setups to use in the regression test.
+   *
+   * @return		the setups
+   */
+  @Override
+  protected Classifier[] getRegressionClassifierSetups() {
+    PEARL PEARLTest = new PEARL();
+    PEARLTest.ensembleSizeOption.setValue(5);
+    PEARLTest.mFeaturesModeOption.setChosenIndex(0);
+    PEARLTest.mFeaturesPerTreeSizeOption.setValue(2);
+
+    return new Classifier[]{
+            PEARLTest,
+    };
+  }
+  
+  /**
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(PEARLTest.class);
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    runTest(suite());
+  }
+}
